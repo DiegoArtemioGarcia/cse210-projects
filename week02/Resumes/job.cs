e6fb4c5fc6da8e3b97d8bda6105a3ee04a604475
DiegoArtemioGarcia <diegoartemiogarcia@icloud.com>
2025-01-28 15:21:03 +0100

    creating job class

---
+++ week02/Resumes/job.cs
@@ -0,0 +1,8 @@
+public class Job
+{
+   public string _name; //save the company name
+   public string _jobTiltle; //save the job title
+   public int _startYear; //save the start year
+    public int _endYear; //save the end year
+
+}
