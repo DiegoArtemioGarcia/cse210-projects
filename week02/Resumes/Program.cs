74ee22f553e7cdfe5f0251cf907e539f892ed55d
DiegoArtemioGarcia <diegoartemiogarcia@icloud.com>
2025-01-28 17:22:16 +0100

    create Program.cs

---
+++ week02/Resumes/Program.cs
@@ -0,0 +1,33 @@
+using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        //create two jobs (job1 and job2) with specific details 
+        //(company, title, years).
+        Job job1 = new Job();
+        job1._company = "Microsoft";
+        job1._jobTitle = "Software Engineer";
+        job1._startYear = 2015;
+        job1._endYear = 2018;
+
+
+        Job job2 = new Job();
+        job2._company = "Google";
+        job2._jobTitle = "Software Developer";
+        job2._startYear = 2018;
+        job2._endYear = 2020;
+
+        //create a resume (resume), assign a name to the person, and add the jobs to their list of employments.
+        Resume myResume = new Resume();
+        myResume._name = "Diego Garcia";
+        
+        myResume._jobs.Add(job1);
+        myResume._jobs.Add(job2);
+
+        //use the DisplayResume() method to show the complete resume.
+        myResume.DisplayResume();
+
+    }
+}
--- week02/Resumes/Resume.cs
+++ week02/Resumes/Resume.cs
@@ -4,7 +4,7 @@ public class Resume
 { 
     public string _name; //save the name
     public List<Job> _jobs = new List<Job>();//List of jobs belonging to the person. Uses a list (List<Job>) to store multiple jobs.
-    public void DisplayResume()
+    public void DisplayResume()//Displays the person's name first, then calls the DisplayJobInfo() method for each one.
     {
         Console.WriteLine($"Name: {_name}");
         foreach (Job job in _jobs)
